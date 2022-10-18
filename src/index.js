c2994b210a739e87bc50218d00039adcd44a2b23
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-17 20:33:12 -0500

    create  card function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,17 @@ async function getData(url) {
     }
 }
 
+const createCardNode=(url)=>{
+    const card=document.createElement("div")
+    const img=document.createElement("img")
+    img.className="mx-auto p-2 rounded-3xl"
+    img.width="300"
+    img.src=url
+    card.appendChild(img)
+    return card
+}
+
+
 
 
 
