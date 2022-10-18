c35e1b7ff1634b740b0a0079720b854b429345a0
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-17 20:31:51 -0500

    getData function from API fecth

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ const URLs={
     randomFox:"https://randomfox.ca/floof/"
 }
 
+async function getData(url) {
+    try {
+        const res=await fetch(url)
+        const data=await res.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+
 
 
