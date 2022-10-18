d5d587b436c712f4a1bdc444adf9ee9760163702
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-17 20:30:34 -0500

    URLs object: Random fox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URLs={
+    randomFox:"https://randomfox.ca/floof/"
+}
 
-console.log('Happy hacking :)')
+
+
